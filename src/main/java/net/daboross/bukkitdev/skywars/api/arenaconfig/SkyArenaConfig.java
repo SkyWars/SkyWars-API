ee815e3d02a8f17242cc77bda9b8ff4f2c379470
Dabo Ross <daboross@daboross.net>
2013-10-22 00:00:19 -0700

    Copy list and make all spawns null

--- src/main/java/net/daboross/bukkitdev/skywars/api/arenaconfig/SkyArenaConfig.java
+++ src/main/java/net/daboross/bukkitdev/skywars/api/arenaconfig/SkyArenaConfig.java
@@ -70,7 +70,10 @@ public class SkyArenaConfig extends Parentable<SkyArenaConfig> implements SkyAre
             throw new IllegalArgumentException("placement-y cannot be smaller than 0.");
         }
         this.arenaName = arenaName;
-        this.rawSpawns = spawns;
+        this.rawSpawns = new ArrayList<SkyPlayerLocation>(spawns.size());
+        for (SkyPlayerLocation l : spawns) {
+            rawSpawns.add(l.changeWorld(null));
+        }
         this.rawNumTeams = numTeams;
         this.rawTeamSize = teamSize;
         this.rawPlacementY = placementY;
