0c2e0744abc5413e65619e43b3f6364a5faddc11
Dabo Ross <daboross@daboross.net>
2014-02-17 03:37:00 -0800

    Add getLocale method for getting set locale

--- src/main/java/net/daboross/bukkitdev/skywars/api/config/SkyConfiguration.java
+++ src/main/java/net/daboross/bukkitdev/skywars/api/config/SkyConfiguration.java
@@ -190,6 +190,7 @@ public interface SkyConfiguration {
      */
     public Pattern getCommandWhitelistCommandRegex();
 
+    public String getLocale();
 //    /**
 //     * Gets whether or not death messages should only be broadcasted to people
 //     * in the arena.
